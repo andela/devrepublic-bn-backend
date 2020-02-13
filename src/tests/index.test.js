0601a045a22ca400686260a62b2fda1bbcb59fea
Baraka <41383503+Baraka-Mugisha@users.noreply.github.com>
2020-02-13 18:14:34 +0200

    feat(i18n): Internationalization (#17)
    install i18n
    configure i18n
    add i18n locales
    [Finishes #171204720]

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -20,3 +20,16 @@ describe('Testing index page ', () => {
       });
   });
 });
+
+describe('Testing index page ', () => {
+  it('should return a message from index page in French.', (done) => {
+    chai.request(app)
+      .get('/api?lang=fr')
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.status).to.equal(200);
+        expect(res.body.message).to.equal('Bienvenue au devRepublic Barefoot Nomad API');
+        done();
+      });
+  });
+});
