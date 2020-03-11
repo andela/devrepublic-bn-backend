af090cf1d7b255e492e34f6b33b9e85524f64196
Jean Bien Aime Byiringiro <jeanaime.byiringiro@Jeans-MacBook-Pro-2.local>
2020-03-11 11:45:40 +0200

    ft(notification): add notifying user for posted comment
    - add a send notification function
    - add tests for the feature
    
    [Finishes #170947568]

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,5 +1,7 @@
 import db from '../models';
 import Response from '../utils/ResponseHandler';
+import notifService from '../services/notificationService';
+import SendNotification from '../utils/sendNotification';
 
 /**
  *
@@ -20,11 +22,31 @@ export default class CommentController {
       const { user } = req;
       const { comment } = req.body;
       const { requestId } = req.params;
+      const { email } = req.managerDetails;
       const newComment = await db.Comments.create({
         requestId,
         commmentOwner: user.id,
         comment,
       });
+      if (user.role === 'manager') {
+        const notifyUser = await notifService.createNotif(req.request.userId, req.request.email, 'your manager posted a comment', '#');
+        const content = {
+          intro: req.__(notifyUser.content),
+          instruction: req.__('Your Manager commented: %s', newComment.comment),
+          text: req.__('View comment'),
+          signature: req.__('signature')
+        };
+        await SendNotification.sendNotif(notifyUser, req, content);
+      } else {
+        const notifyUser = await notifService.createNotif(req.request.managerId, email, 'your requester posted a comment', '#');
+        const content = {
+          intro: req.__(notifyUser.content),
+          instruction: req.__('Your requester commented: %s', newComment.comment),
+          text: req.__('View comment'),
+          signature: req.__('signature')
+        };
+        await SendNotification.sendNotif(notifyUser, req, content);
+      }
       return Response.success(res, 200, res.__('Comment is successfully posted'), newComment);
     } catch (error) {
       return Response.errorResponse(res, 500, res.__('server error'));
--- src/tests/comments.test.js
+++ src/tests/comments.test.js
@@ -1,17 +1,48 @@
 import chai from 'chai';
+import dotenv from 'dotenv';
 import chaiHttp from 'chai-http';
-import app from '../index';
+import sinon from 'sinon';
+import ioClient from 'socket.io-client';
+import sgMail from '@sendgrid/mail';
+import app, { server } from '../index';
+
+import { provideToken } from '../utils/tokenHandler';
 
 const {
   expect
 } = chai;
-
+dotenv.config();
 let requesterToken, managerToken, wrongManagerToken;
 const requestId = '51e74db7-5510-4f50-9f15-e23710331ld5';
 const wrongRequestId = '51e74db7-5510-4f50-9f15-e23710331ld555';
 chai.use(chaiHttp);
 
 describe('COMMENTS TESTS', () => {
+  const socketToken = provideToken('79660e6f-4b7d-4g21-81re-74f54e9e1c8a', true, 'jeanne@andela.com', 'requester');
+  let clientSocket;
+  const BASE_URL = process.env.BASE_URL || `http://localhost:${server.address().port}`;
+  beforeEach((done) => {
+    sinon.stub(sgMail, 'send').resolves({
+      to: 'jeanaime@andela.com',
+      from: 'devrepublic.team@gmail.com',
+      subject: 'barefoot nomad',
+      html: 'this is stubbing message'
+    });
+    clientSocket = ioClient.connect(BASE_URL, {
+      transportOptions: {
+        polling:
+      { extraHeaders: { token: socketToken } }
+      },
+      'force new connection': true,
+      forceNew: true,
+    });
+    done();
+  });
+  afterEach((done) => {
+    clientSocket.disconnect();
+    sinon.restore();
+    done();
+  });
   it('should login a user who will comment on a request', (done) => {
     chai
       .request(app)
@@ -79,8 +110,14 @@ describe('COMMENTS TESTS', () => {
         expect(res.body.data.requestId).to.equal('51e74db7-5510-4f50-9f15-e23710331ld5');
         expect(res.body.data.commmentOwner).to.equal('0119b84a-99a4-41c0-8a0e-6e0b6c385165');
         expect(res.body.data.comment).to.equal('As a your manager I can add any comment on your request. I can also cancel or approve your request but let me comment first');
-        done();
       });
+    clientSocket.on('notification', (msg) => {
+      expect(JSON.parse(msg)).to.be.an('object');
+      expect(JSON.parse(msg).receiverId).to.equal('79660e6f-4b7d-4g21-81re-74f54e9e1c8a');
+      expect(JSON.parse(msg).status).to.equal('unread');
+      expect(JSON.parse(msg).content).to.equal('your manager posted a comment');
+      done();
+    });
   });
   it('should not allow a manager to comment on a request which does not belong to him', (done) => {
     chai
