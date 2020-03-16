93fb0dd6da288e8b6fb44fc0036f26b1f369244a
Jim Ntare <jim.ntare@gmail.com>
2020-03-16 17:47:49 +0200

    ft(users are able to chat):
    add chat.html page
    add chat functionality to the ioMiddleware
    
    [ #170947581 in progress]

---
+++ src/migrations/20200315174530-create-chat.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Chats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      message: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Chats');
+  }
+};
